275cafd9a9b55115f086da58749905815558b1d5
RussellTheRush <RussellTheRush@gmail.com>
2014-11-26 11:32:58 +0800

    bignumber bugfix

--- libperm_rsa.h
+++ libperm_rsa.h
@@ -9,4 +9,5 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 int libperm_rsa_modPow(u8 *msg, u32 msgBytes, u8 *strE, u32 strELen, u8 *strN, u32 strNLen, u8 *outStr);
 void dump_bytes(u8 *a, u32 l);
+void printBytesAsHexString(u8 *bytes, u32 size);
 #endif
--- test_libperm_rsa.c
+++ test_libperm_rsa.c
@@ -7,7 +7,7 @@
 #define STR_d       "18D367AD259001C7EBA934766F2A0181CEF1B894927757613CD68C679B57C8D2808DCADC80E708958EAF2B2F82CA55B9979B6E0EAB2284CE2ADE9C45E5E2EC2D"
 
 int main(int argc, char *argv[]) {
-    char msg[] = {0x21,};
+    char msg[] = {0x21,0x11};
     char res[N_BYTE_SIZE];
     char strN[] = STR_N;
     char strE[] = STR_e;
@@ -20,22 +20,22 @@ int main(int argc, char *argv[]) {
     int strDLen = strlen(strD);
     u32 i;
 
-    for (i=0; i<0xffffff; i++) {
+    //for (i=0; i<0xffffff; i++) {
 
-        res_size = libperm_rsa_modPow((u8 *)&i, 4, strE, strELen, strN, strNLen, res);
+        res_size = libperm_rsa_modPow((u8 *)msg, sizeof(msg), strE, strELen, strN, strNLen, res);
         dump_bytes(res, res_size);
         res_size = libperm_rsa_modPow(res, res_size, strD, strDLen, strN, strNLen, res);
 
 
         dump_bytes(res, res_size);
 
-        if (memcmp((void *)&i, res, res_size) == 0) {
+        if (memcmp((void *)msg, res, res_size) == 0) {
             printf("#successed.\n");
         } else {
             fprintf(stderr, "#failed, i: %d\n", i);
             printf("#failed, i: %d\n", i);
         }
-    }
+    //}
 
     return 0;
 }
